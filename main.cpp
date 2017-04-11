6448fbb4f009ba5e94430b5d24ac4db352f9c360
yoyoman1212 <nedrigal@gmail.com>
2017-04-11 12:33:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
-#include "bryce.h";
-#include "adam.h";
+//ConsoleApplication1.cpp : Defines the entry point for the console application.
 #include "stdafx.h"
 #include <iostream>
-#include <string>
-#include "Colour.h" // For coloured text
+
+#include "bryce.h";
+#include "adam.h";
 
 using namespace std;
 
-int main(){
+int main(int argc, char* c[], int l) {
+	
+	if (argc < 3) {
+		cerr << "Usage: " << c[0] << " LETTER #LINES" << endl;
+		return 1;
+	}
 
-  
+	b_main();
+
+	a_main();
+
+	return 0;
 
 }
