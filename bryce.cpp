8481aac4a361e34bcee4f56f6970ded596adaf92
yoyoman1212 <nedrigal@gmail.com>
2017-04-11 19:58:41 -0400

    Update bryce.cpp

--- bryce.cpp
+++ bryce.cpp
@@ -31,20 +31,9 @@ int b_main(char search_key) {
 		cout << "Error: File not open" << endl;
 		return 2;
 	}
-	for (int i = 0; i < 24; i++) {
-
-		cout << characters[i] << endl;
-
-	}
-
-	cout << "Searching for characters..." << endl;
 
 	char_search(search_key);
 
-	for (int i = 0; i < 24; i++) {
-		if (search_results[i] != "")
-			cout << search_results[i] << endl;
-	}
 	return 0;
 
 }
@@ -54,7 +43,6 @@ void char_search(char key) {
 	for (int i = 0; i < 24; i++) {
 
 		if (characters[i].find_first_of(key) != -1) {
-			//cout << characters[i] << endl;
 			search_results[counter] = characters[i];
 			counter++;
 		}
