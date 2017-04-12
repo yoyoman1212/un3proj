0e4e488eae40e515405239ede6890658a61cc05f
SmokingFlash <bluekillers700@gmail.com>
2017-04-12 19:20:42 -0400

    Update adam.cpp

--- adam.cpp
+++ adam.cpp
@@ -7,22 +7,22 @@
 
 using namespace std; 
 
-int a_main(int count, char dis_size, string sresult[24]) {
-
-	int atoi(char dis_size);
+int a_main(int count, int dis_size, string sresult[24]) { 
 
 	string* found_chars = new string [count] ;
 
 	for (int i = 0; i < count; i++)  // count is brough from Bryce's file to know the number of characters that filled searhc parameters
 		found_chars[i] = sresult[i]; // To not have oversized array copy all elements into new array
 	
-	for (int i = 0; i < dis_size - 48; i++) { // Number was always 48 greater than command prompt number,
-										      // solution was removing value of 48 each time
-		if (found_chars[i] != "0") 
+	for (int i = 0; i < dis_size; i++) { // Number was always 48 greater than command prompt number,
+										 // solution was removing value of 48 each time
+		if (i == count) // makes sure that the maximum number of elements printed is equal to the number of search results or count
+			return 0;
+
+		else
 			cout << green << found_chars[i] << white << endl; //Added green to make the name "pop" more
 
-		else if (found_chars[i] == "0") // combination of this and previous if statement only print the number of 
-			return 0;                   // character from search results defined by the command prompt 
+		
 
 	}
 
