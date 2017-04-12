899b79a17c1d827e47c2f89f14256506c4d3b488
SmokingFlash <bluekillers700@gmail.com>
2017-04-12 19:26:13 -0400

    Update adam.cpp

--- adam.cpp
+++ adam.cpp
@@ -14,8 +14,8 @@ int a_main(int count, int dis_size, string sresult[24]) {
 	for (int i = 0; i < count; i++)  // count is brough from Bryce's file to know the number of characters that filled searhc parameters
 		found_chars[i] = sresult[i]; // To not have oversized array copy all elements into new array
 	
-	for (int i = 0; i < dis_size; i++) { // Number was always 48 greater than command prompt number,
-										 // solution was removing value of 48 each time
+	for (int i = 0; i < dis_size; i++) { 
+		
 		if (i == count) // makes sure that the maximum number of elements printed is equal to the number of search results or count
 			return 0;
 
