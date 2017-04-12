213c8205dc500af42a5f7ffc89d7cdbeb0863745
yoyoman1212 <nedrigal@gmail.com>
2017-04-12 19:45:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//ConsoleApplication1.cpp : Defines the entry point for the console application.
+//main.cpp : Defines the entry point for the console application.
 #include "stdafx.h";
 #include <iostream>
 #include <ctype.h>
