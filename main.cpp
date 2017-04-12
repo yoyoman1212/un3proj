bdacaad4619903beec3f6139dddfde19eff8fbd4
yoyoman1212 <nedrigal@gmail.com>
2017-04-12 19:34:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,10 +16,13 @@ char search_key;
 int main(int argc, char* c[]) {
 
 	int search_fails = 0;
+	int search_fails_2 = 0;
 
 	char accepted_chars[] = "abcdefghijklmnopqrstuvwxyz:76.";
+	char accepted_chars_2[] = "0123456789";
 
 	char letter = tolower(*c[1]);
+	char number = *c[2];
 
 	if (argc < 3) {
 		cerr << "Usage: " << c[0] << " letter #lines" << endl;
@@ -31,9 +34,19 @@ int main(int argc, char* c[]) {
 			search_fails++;
 
 	if (search_fails == 30) {
-		cerr << "Error: Invalid character entered." << endl;
+		cerr << "Error: Invalid character entered for arg 1." << endl;
 		return 3;
 	}
+
+	for (int i = 0; i < 10; i++)
+		if (number != accepted_chars_2[i])
+			search_fails_2++;
+
+	if (search_fails_2 == 10) {
+		cerr << "Error: Invalid character entered for arg 2." << endl;
+		return 4;
+	}
+
 
 	int numlines;
 	numlines = atoi(c[2]);
