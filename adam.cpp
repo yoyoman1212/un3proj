9189b64ef7db707526306781c227108b7f338c71
SmokingFlash <bluekillers700@gmail.com>
2017-04-12 20:15:03 -0400

    Update adam.cpp

--- adam.cpp
+++ adam.cpp
@@ -13,25 +13,25 @@ int a_main(int count, int dis_size, string sresult[24]) {
 	string select;
 
 
-	for (int i = 0; i < count; i++)  // count is brough from Bryce's file to know the number of characters that filled searhc parameters
+	for (int i = 0; i < count; i++)  // count is brough from Bryce's file to know the number of characters that filled search parameters
 		found_chars[i] = sresult[i]; // To not have oversized array and be able ot do tail copy all elements into new array
 	
 	cout << blue << "Would you like to display " << yellow << "head" << blue << " with " << count << " elements or " << yellow << "tail" << blue
 		<< " with " << count << " elements?: " << green; // head and tail highlighed in different colours as they are options
 	cin >> select; 
 
-	if (select == "head")
+	if (select == "head") // first 'dis_size" elements of the array
 		for (int i = 0; i < dis_size; i++) {
 
 			if (i == count) // makes sure that the maximum number of elements printed is equal to the number of search results or count
 				return 0;
 
 			else
-				cout << green << found_chars[i] << white << endl; //Added green to make the elements "pop" more
+				cout << green << found_chars[i] << white << endl;
 
 		}
 
-	else if (select == "tail")
+	else if (select == "tail") // last 'dis_size" elements of the array
 		for (int i = count - 1; i > -1; i--) { // -1 because array starts at[0] not [1]
 
 			if (i == dis_size - 1) // - 1 because array starts at [0] not [1]
@@ -41,7 +41,7 @@ int a_main(int count, int dis_size, string sresult[24]) {
 				cout << green << found_chars[i] << white << endl;
 		}
 
-	else {
+	else { // if input is not "head" or "tail"
 		cout << red << "That was not a valid selection" << endl;
 		cout << "Ending Program..." << white << endl;
 		return 0;
